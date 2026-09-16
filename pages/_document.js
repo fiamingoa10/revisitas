798a6890c38267e63e947c8d0266eb8fc85a86dd
ale fiamingo <you@example.com>
2026-09-15 23:19:30 -0300

    Configuro manifest y favicon en _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,21 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head>
+        {/* Manifest de la PWA */}
         <link rel="manifest" href="/manifest.webmanifest" />
+
+        {/* Ícono/Favicon */}
         <link rel="icon" href="/__grok/revisitas.png" type="image/png" />
+
+        {/* Color de tema (barra superior en móviles) */}
         <meta name="theme-color" content="#000000" />
+
+        {/* Opcional: descripción de la app */}
+        <meta name="description" content="Aplicación Revisitas - PWA" />
       </Head>
       <body>
         <Main />
