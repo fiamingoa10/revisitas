90d6d79420153320a6167c3e9cc217db35f38322
ale fiamingo <you@example.com>
2026-09-15 22:56:10 -0300

    Agrego _document.js con manifest y favicon

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/__grok/revisitas.png" type="image/png" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
