ec08d337cd95c8f49ee0381089c69a71c06e582a
ale fiamingo <you@example.com>
2026-09-16 15:12:00 -0300

    Corrijo configuración PWA sin duplicados

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-const withPWA = require("next-pwa")({
-  dest: "public",       // dónde se guarda el service worker
-  register: true,       // registra automáticamente el SW
-  skipWaiting: true     // activa el SW nuevo sin esperar
-});
-
-module.exports = withPWA({
-  reactStrictMode: true // o cualquier otra config que ya tengas
-});
+// next.config.js
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: false, // asegurate de que esté habilitado
+  disable: false,
+  buildExcludes: [/middleware-manifest\.json$/],
 });
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
