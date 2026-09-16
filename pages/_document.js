80f9a313245d7d7a3984657ef0b414d24d84cb20
ale fiamingo <you@example.com>
2026-09-16 15:05:05 -0300

    Optimizo manifest y meta tags PWA Revisitas

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,19 @@ export default function Document() {
     <Html lang="es">
       <Head>
         {/* Manifest de la PWA */}
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="manifest" href="/manifest.webmanifest" crossorigin="use-credentials" />
 
         {/* Ícono/Favicon */}
-        <link rel="icon" href="/revisitas.png" type="image/png" />
+        <link rel="icon" href="/revistas.png" type="image/png" />
 
         {/* Color de tema */}
         <meta name="theme-color" content="#000000" />
+
+        {/* Compatibilidad con iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Revisitas" />
+        <link rel="apple-touch-icon" href="/revistas.png" />
 
         {/* Descripción opcional */}
         <meta name="description" content="Aplicación Revisitas - PWA" />
