c76c2fc0039d9433b02c00db3ceb5a454cfd4328
ale fiamingo <you@example.com>
2026-09-18 18:25:47 -0300

    Fix: remover eslint de next.config y agregar _document.tsx

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     // Evita que Vercel cancele el build por errores de TypeScript
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Evita bloqueos por advertencias de ESLint
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
