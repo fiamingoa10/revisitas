fdd2bc3d68196ccfc4cae83ccace74ebe9a36537
ale fiamingo <you@example.com>
2026-09-24 15:09:21 -0300

    Fix: agregar estructura correcta en la carpeta pages

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    
+      
+        {/* Manifest para PWA */}
+        
+
+        {/* Ícono de la aplicación (favicon) */}
+        
+
+        {/* Color de la barra del navegador */}
+        
+      
+      
+        
+        
+      
+    
+  );
+}
