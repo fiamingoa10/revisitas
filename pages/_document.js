733ca8d3efec776b16728bac15c1fb4fce3b2c08
ale fiamingo <you@example.com>
2026-09-24 19:54:19 -0300

    Fix: corregir sintaxis completa en 404, index y _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return ( 
+  return (
+    
+      
+        
+        
+        
+      
+      
+        
+        
+      
+    
   );
 }
-        
         
         
       
